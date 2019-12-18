139764b02aba75e924c08a66b100ecab995113f6
Willou <lafarie.william@gmail.com>
2019-12-18 20:05:13 +0100

    Setup djangoFrameworkRest + API connection with the first model

---
+++ hatvp/serializers.py
@@ -0,0 +1,31 @@
+# serializers.py
+from rest_framework import serializers
+
+from .models import Informations_generales
+
+
+class InfoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Informations_generales
+        fields = (
+            'representants_id',
+            'adresse',
+            'code_postal',
+            'derniere_publication_activite',
+            'date_premiere_publication',
+            'declaration_organisation_appartenance',
+            'declaration_tiers',
+            'denomination',
+            'identifiant_national',
+            'activites_publiees',
+            'page_facebook',
+            'page_linkedin',
+            'page_twitter',
+            'site_web',
+            'nom_usage_HATVP',
+            'pays',
+            'sigle_HATVP',
+            'type_identifiant_national',
+            'ville',
+            'label_categorie_organisation'
+        )
