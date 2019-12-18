fd74f34bd9eb91ad702e5eccf357cf41895689ad
Willou <lafarie.william@gmail.com>
2019-12-18 11:22:07 +0100

    hotfix

--- hatvp_API/settings.py
+++ hatvp_API/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'hatvp.apps.HatvpConfig',
+    'hatvp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
