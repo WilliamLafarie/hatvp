5e0039fc963083f8bd1949f84d44f9f1e491bf59
gauthier <gauthier.magne@hetic.net>
2019-12-18 16:29:14 +0100

    Massif push

--- hatvp/admin.py
+++ hatvp/admin.py
@@ -1,28 +1,264 @@
 from django.contrib import admin
 from .models import *
 from django.contrib import admin
+from django.db.models.fields.related import *
+from django.db.models import *
 import csv
 import glob
 import os
+import re
 from os.path import basename, splitext
+import sys
+from datetime import datetime
 
-
-Informations_generales.objects.all().delete()
-Dirigeants.objects.all().delete()
-info_gen=['representants_id', 'adresse', 'code_postal', 'derniere_publication_activite', 'date_premiere_publication', 'declaration_organisation_appartenance', 'declaration_tiers', 'denomination', 'identifiant_national','activites_publiees','page_facebook', 'page_linkedin', 'page_twitter', 'site_web','nom_usage_HATVP', 'pays', 'sigle_HATVP', 'type_identifiant_national', 'ville', 'label_categorie_organisation']
-class reader_test():
-    with open('fichierCsv/1_Informations_generales.csv',encoding="utf8") as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=';')
-        next(spamreader,None)
-        for row in spamreader:
-            obj = Informations_generales()
-            for tab in range(len(info_gen)):
-                setattr(obj, info_gen[tab],row[tab])
-                obj.save()
-    print("--------------- Fin remplissage reader test ---------------")
 
 
 '''
+Informations_generales,Dirigeants,Collaborateurs
+Clients
+Affiliations
+Niveaux_intervention
+Exercices
+Objets_activites
+Domaines_intervention
+Observations
+Decisions_concernees
+Beneficiaires
+Actions_menees
+
+
+
+def date_function(value,type):
+    if value=="":
+        return None
+    if type == 1:
+        return datetime.strptime(str(value),'%d/%m/%Y %H:%M:%S')
+    if type == 2:
+        return datetime.strptime(str(value),'%d/%m/%Y')
+        pass
+
+
+def trieFichierModel(listFichier):
+    i=0
+    compteur1=1
+    compteur2=0
+    modelListInter=[]
+    listFichierTrie=[]
+    fileInOrder=[]
+    while(i==0):
+        fileToTest = listFichier[compteur2].split("_",1)
+        if(int(fileToTest[0]) == compteur1):
+            modelListInter = fileToTest[1].split('.')
+            fileInOrder.append(listFichier[compteur2])
+            listFichierTrie.append(modelListInter[0])
+            compteur1+=1
+            compteur2 = 0
+        elif(compteur1 == len(listFichier)):
+            return listFichierTrie,fileInOrder
+            i=1
+        else:
+            compteur2+=1
+
+def destroyData(listModels):
+    for lenList in range(len(listModels)):
+        listModels[lenList].objects.all().delete()
+
+
+class Foobar:
+    pass
+
+def str_to_class(list):
+    modelsObject=[]
+    for lenList in  range(len(list)):
+        modelsObject.append(getattr(sys.modules[__name__], list[lenList]))
+        #print(modelsObject)
+    return modelsObject
+
+def recupChamp():
+    listInfo=Collaborateurs._meta.get_fields()
+    for i in range(len(listInfo)):
+        if isinstance(listInfo[i],ManyToOneRel):
+             #if hasattr(listInfo[7],'attname'):
+            pass
+        else:
+            #print(i)
+            listChamps.append(listInfo[i].attname)
+    return listChamps
+
+def recupChampList(listModels):
+    listChamps=[[]]
+    listIlistInfo=[]
+    dico = {}
+    for nbModels in range(len(listModels)):
+        listInfo=listModels[nbModels]._meta.get_fields()
+        #print(listIlistInfo)
+        listIlistInfo=[]
+        for nbInfo in range(len(listInfo)):
+            if(hasattr(listInfo[nbInfo],'attname')):
+                if(listInfo[nbInfo].attname !='id'):
+                    listIlistInfo.append(listInfo[nbInfo].attname)
+            else:
+                pass
+        dico[listModels[nbModels]] = listIlistInfo
+    return dico
+
+def reader_test(listeFichier,dicoModels):
+    listRefus = [Informations_generales(),Exercices(),Objets_activites(),Observations()]
+    #for nbFichier in range(len(listeFichier)):
+    for nbFichier,fichier in enumerate(listeFichier):
+        #print(listeFichier)
+        fichier = listeFichier[nbFichier]
+       #objetNew = classModels[nbFichier]()
+        allChamps = dicoModels[classModels[nbFichier]]
+        print("L'INDICE EST : {}".format(nbFichier))
+        #print('OBJET UTILISE : {}'.format(objetNew))
+        print("CHAMPS SONT  : {}".format(allChamps))
+        with open('fichierCsv/'+fichier,encoding="utf8") as csvfile:
+           
+            print("--------------- DEBUT remplissage FICHIER {}  DANS reader test ---------------".format(fichier))
+            #read_file = reader(fichier)
+            #apps_data = list(read_file)
+            #rowcount = len(apps_data) #which incudes header row
+            #print("Total rows incuding header: " + str(rowcount))
+            spamreader = csv.reader(csvfile, delimiter=';')
+            next(spamreader,None)
+            #i=0
+            for row in spamreader:
+                objetNew = classModels[nbFichier]()
+                print("Object SELECTIONNE : {} ".format(objetNew))
+                #i = i+1
+                #print("LECTURE LIGNE : {}".format(i))
+                for tab in range(len(allChamps)):
+                    
+                    type_variable=objetNew._meta.get_field(allChamps[tab]) #Nom du champs
+                    if isinstance(type_variable,models.DateTimeField):
+                        if(row[tab] ==""):
+                            row[tab]=None
+                        else :
+                            try:
+                                row[tab] = datetime.strptime(row[tab], "%d/%m/%Y %H:%M:%S")
+                            except ValueError: # strptime raises a ValueError
+                                row[tab] = datetime.strptime(row[tab], "%Y-%m-%d")
+                    
+                    #row[tab] = date_function(row[tab],1)
+                    #print(len(allChamps))
+                    print("AFFICHAGE LIGNE : {}".format(row[tab]))
+                    #print('voici TOUT LES CHAMPS DE : {} --------- : {}'.format(classModels[nbFichier],allChamps))
+                    #if(objetNew not in listRefus):
+                       # print('PARCOURIR LES CHAMPS UTILISES : {}'.format(allChamps[tab]))   
+                       # print('OBJECT UTILISE  {}'.format(objetNew))
+                    if(allChamps[tab] == 'representants_id_id'):
+                        print("---------------DEDDAANNS  VALUE : {}".format(Info_generales.objects.get(representants_id = row[tab])))
+                        row[tab] = Informations_generales.objects.get(representants_id = row[tab])
+                    if(tab == 'exercices_id'):
+                        row[tab] = Exercices.objects.get(exercices_id = row[tab])
+                    if(tab == 'activite_id'):
+                        row[tab] = Objets_activites.objects.get(activite_id = row[tab])
+                    if(tab == 'action_representation_interet_id'):
+                        row[tab] = Observations.objects.get(action_representation_interet_id = row[tab])
+                    
+                    print('OBJECT UTILISE  {}'.format(objetNew))   
+                    setattr(objetNew, allChamps[tab],row[tab])
+                    
+                objetNew.save()
+               # print(objetNew)
+        csvfile.close()
+                #print('Nombre de ligner : {}'.format(len(row)))
+        #print("--------------- Fin remplissage  FICHIER {}  DANS reader test ---------------".format(fichier))
+
+#listvide  = recupChamp()
+#print(listvide)
+fichier=[]
+listFichier = os.listdir('./fichierCSV')
+#print(listFichier)
+dicoModels={}
+listModel,fichierTrie = trieFichierModel(listFichier) #Recuperation des noms des models par ordre
+classModels = str_to_class(listModel) # Transformations des strings models en classe
+#print(listTrie)
+destroyData(classModels) #Destruction des datas de toutes les bases de donnes
+dicoModels = recupChampList(classModels)
+#print(dicoModels.keys())
+#print(listInfoModels)
+reader_test(fichierTrie,dicoModels)
+
+
+info_gen=['representants_id', 'adresse', 'code_postal', 'derniere_publication_activite', 'date_premiere_publication', 'declaration_organisation_appartenance', 'declaration_tiers', 'denomination', 'identifiant_national','activites_publiees','page_facebook', 'page_linkedin', 'page_twitter', 'site_web','nom_usage_HATVP', 'pays', 'sigle_HATVP', 'type_identifiant_national', 'ville', 'label_categorie_organisation']
+
+class reader_test():
+    
+    #listFichier = os.listdir('./fichierCSV')
+    #for nbFichier in range(0,2):
+        #objetNew = classModels[nbFichier]()
+    with open('fichierCsv/1_Informations_generales.csv',encoding="utf8") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        next(spamreader,None)
+        i=0
+        for row in spamreader:
+            objetInformations = Informations_generales()
+            i = i+1
+            print(i)
+            for tab in range(len(info_gen)):
+                setattr(objetInformations, info_gen[tab],row[tab])
+            objetInformations.save()
+    print("--------------- Fin remplissage reader test ---------------")   
+#listvide = []
+
+def modelsName():
+    modeslListFinal=[]
+    modeslListIntermediaire=[]
+    listFichier = os.listdir('./fichierCSV')
+    listOrder=[]
+    for nbFichier in range(len(listFichier)):
+        modeslListIntermediaire = listFichierTrie[nbFichier].split("_",1)
+        modeslListIntermediaire = modeslListIntermediaire[1].split('.')
+        modeslListFinal.append(modeslListIntermediaire[0])
+    #print(modeslListFinal)
+#print(Info_generales._meta.get_fields())
+
+resultat= 0
+resultat = dicoModels[Informations_generales]
+for i in range(len(resultat)):
+    print("Compteur : {} , et sa valeur : {}".format(i,resultat[i]))
+#print(dicoModels[Informations_generales])
+
+#def recuperation
+#print(models[1].enums_all())
+
+
+    #if hasattr(listInfo[7],'attname'):
+#Informations_generales.objects.all().delete()
+#Dirigeants.objects.all().delete()
+info_gen=['representants_id', 'adresse', 'code_postal', 'derniere_publication_activite', 'date_premiere_publication', 'declaration_organisation_appartenance', 'declaration_tiers', 'denomination', 'identifiant_national','activites_publiees','page_facebook', 'page_linkedin', 'page_twitter', 'site_web','nom_usage_HATVP', 'pays', 'sigle_HATVP', 'type_identifiant_national', 'ville', 'label_categorie_organisation']
+
+reader
+
+
+class reader_dirigeants():
+    with open('fichierCsv/2_dirigeants.csv',encoding="utf8") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        next(spamreader,None)
+        for row in spamreader:
+            obj2 = Dirigeants()
+            for tab in range(len(dirig)):
+                if(row[tab] == 'representants_id'):
+                    print("Ici primaire Kay")
+                    print("value row tab{}".format(row[tab]))
+                    row[tab] = Info_generales.objects.get(representants_id = row[tab])
+                setattr(obj2, dirig[tab],row[tab])
+                #print("TABLEAU REMPLIS :  {}".format(info_gen[tab]))
+                #print('LES INFOS QUI REMPLISSENT : {}'.format(row[tab]))
+                obj2.save()
+
+                #print(obj)
+   #if(champs_exercice[i]" == 'representans-id'):
+#    row[i] = Info_generales.objects.get(representans_id = row[i])
+
+
+
+
+
+
+
 
 #filepath = glob.glob('./fichierCSV/*.txt')
 #DefaultStorage.listdir(filepath)
@@ -96,4 +332,36 @@ class reader_dirigeants():
                     #print("TABLEAU REMPLIS :  {}".format(info_gen[tab]))
                     #print('LES INFOS QUI REMPLISSENT : {}'.format(row[tab]))
                     obj2.save()
-                    '''
+                  
+
+
+ VERSIONS NOM OPE RECUPERATIONS MODELS
+def modelsName():
+    modeslListFinal=[]
+    modeslListIntermediaire=[]
+    listFichier = os.listdir('./fichierCSV')
+    listOrder=[]
+    for nbFichier in range(len(listFichier)):
+        modeslListIntermediaire = listFichierTrie[nbFichier].split("_",1)
+        modeslListIntermediaire = modeslListIntermediaire[1].split('.')
+        modeslListFinal.append(modeslListIntermediaire[0])
+    #print(modeslListFinal)
+#print(Info_generales._meta.get_fields())
+
+
+
+def reader_test():
+    objetInformations = Informations_generales()
+    listFichier = os.listdir('./fichierCSV')
+    for nbFichier in range(0,2):
+        objetNew = classModels[nbFichier]()
+        with open('fichierCsv/'+listFichier[nbFichier],encoding="utf8") as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=';')
+            next(spamreader,None)
+            for row in spamreader:
+                for tab in range(len(info_gen)):
+                    setattr(objetNewbj, info_gen[tab],row[tab])
+                    objetNew.save()
+        print("--------------- Fin remplissage reader test ---------------")
+
+        '''
--- hatvp/models.py
+++ hatvp/models.py
@@ -4,108 +4,112 @@ from django.db import models
 
 class Informations_generales(models.Model):
     representants_id = models.IntegerField(primary_key=True)
-    adresse = models.CharField(max_length=200)
-    code_postal = models.CharField(max_length=200)
-    derniere_publication_activite = models.CharField(max_length=200) #A CHANGER EN DATE
-    date_premiere_publication = models.CharField(max_length=200) #A CHANGER EN DATE
+    adresse = models.CharField(null=True,blank=True,max_length=200)
+    code_postal = models.CharField(null=True,blank=True,max_length=200)
+    derniere_publication_activite = models.DateTimeField(null=True,blank=True) #A CHANGER EN DATE models.DateTimeField(null=True,blank=True,null=True,blank=True)
+    date_premiere_publication = models.DateTimeField(null=True,blank=True) #A CHANGER EN DATE
     declaration_organisation_appartenance = models.BooleanField(null=True,blank=True)
     declaration_tiers = models.BooleanField(null=True,blank=True)
-    denomination = models.CharField(max_length=200)
-    identifiant_national = models.CharField(max_length=200)
+    denomination = models.CharField(null=True,blank=True,max_length=200)
+    identifiant_national = models.CharField(null=True,blank=True,max_length=200)
     activites_publiees =  models.BooleanField(null=True,blank=True)
-    page_facebook =models.CharField(max_length=200)
-    page_linkedin=models.CharField(max_length=200)
-    page_twitter=models.CharField(max_length=200)
-    site_web=models.CharField(max_length=200)
-    nom_usage_HATVP=models.CharField(max_length=200)
-    pays=models.CharField(max_length=200)
-    sigle_HATVP=models.CharField(max_length=200)
-    type_identifiant_national=models.CharField(max_length=200)
-    ville=models.CharField(max_length=200)
-    label_categorie_organisation=models.CharField(max_length=200)
+    page_facebook =models.CharField(null=True,blank=True,max_length=200)
+    page_linkedin=models.CharField(null=True,blank=True,max_length=200)
+    page_twitter=models.CharField(null=True,blank=True,max_length=200)
+    site_web=models.CharField(null=True,blank=True,max_length=200)
+    nom_usage_HATVP=models.CharField(null=True,blank=True,max_length=200)
+    pays=models.CharField(null=True,blank=True,max_length=200)
+    sigle_HATVP=models.CharField(null=True,blank=True,max_length=200)
+    type_identifiant_national=models.CharField(null=True,blank=True,max_length=200)
+    ville=models.CharField(null=True,blank=True,max_length=200)
+    label_categorie_organisation=models.CharField(null=True,blank=True,max_length=200)
 
 class Dirigeants(models.Model):
-    civilite_dirigeant=models.CharField(max_length=200)
-    fonction_dirigeant=models.CharField(max_length=200)
-    nom_dirigeant=models.CharField(max_length=200)
-    prenom_dirigeant=models.CharField(max_length=200)
+    civilite_dirigeant=models.CharField(null=True,blank=True,max_length=200)
+    fonction_dirigeant=models.CharField(null=True,blank=True,max_length=200)
+    nom_dirigeant=models.CharField(null=True,blank=True,max_length=200)
+    prenom_dirigeant=models.CharField(null=True,blank=True,max_length=200)
     representants_id = models.ForeignKey(Informations_generales,on_delete=models.CASCADE)
-    nom_prenom_dirigeant=models.CharField(max_length=200)
+    nom_prenom_dirigeant=models.CharField(null=True,blank=True,max_length=200)
 
 class Collaborateurs(models.Model):
-    civilite_collaborateur=models.CharField(max_length=200)
-    fonction_collaborateur=models.CharField(max_length=200)
-    nom_collaborateur=models.CharField(max_length=200)
-    prenom_collaborateur=models.CharField(max_length=200)
+    civilite_collaborateur=models.CharField(null=True,blank=True,max_length=200)
+    fonction_collaborateur=models.CharField(null=True,blank=True,max_length=200)
+    nom_collaborateur=models.CharField(null=True,blank=True,max_length=200)
+    prenom_collaborateur=models.CharField(null=True,blank=True,max_length=200)
     representants_id = models.ForeignKey(Informations_generales,on_delete=models.CASCADE)
-    nom_prenom_collaborateur=models.CharField(max_length=200)
+    nom_prenom_collaborateur=models.CharField(null=True,blank=True,max_length=200)
 
 class Clients(models.Model):
     representants_id = models.ForeignKey(Informations_generales,on_delete=models.CASCADE)
-    denomination_client=models.CharField(max_length=200)
-    identifiant_national_client=models.CharField(max_length=200)
-    type_identifiant_national_client=models.CharField(max_length=200)
+    denomination_client=models.CharField(null=True,blank=True,max_length=200)
+    identifiant_national_client=models.CharField(null=True,blank=True,max_length=200)
+    type_identifiant_national_client=models.CharField(null=True,blank=True,max_length=200)
 
 class Affiliations(models.Model):
     representants_id = models.ForeignKey(Informations_generales,on_delete=models.CASCADE)
-    denomination_affiliation=models.CharField(max_length=200)
-    identifiant_national_affiliation=models.CharField(max_length=200)
-    type_identifiant_national_affiliation=models.CharField(max_length=200)
+    denomination_affiliation=models.CharField(null=True,blank=True,max_length=200)
+    identifiant_national_affiliation=models.CharField(null=True,blank=True,max_length=200)
+    type_identifiant_national_affiliation=models.CharField(null=True,blank=True,max_length=200)
 
 class Niveaux_intervention(models.Model):
-    niveau_intervention=models.CharField(max_length=200)
+    niveau_intervention=models.CharField(null=True,blank=True,max_length=200)
     representants_id = models.ForeignKey(Informations_generales,on_delete=models.CASCADE)
 
 class Exercices(models.Model):
     exercices_id = models.IntegerField(primary_key=True)
     representants_id = models.ForeignKey(Informations_generales,on_delete=models.CASCADE)
-    date_debut = models.CharField(max_length=200) #A CHANGER EN DATE
-    date_fin = models.CharField(max_length=200) #A CHANGER EN DATE
-    exercice_sans_activite=models.BooleanField()
-    nombre_activites= models.IntegerField()
-    declaration_incomplete=models.BooleanField()
-    date_publication = models.CharField(max_length=200) #A CHANGER EN DATE
-    exercice_sans_CA=models.BooleanField()
-    montant_depense=models.CharField(max_length=200)
-    nombre_salaries=models.FloatField()
-    chiffre_affaires = models.CharField(max_length=200)
-    annee_debut= models.IntegerField()
-    annee_fin= models.IntegerField()
-    montant_depense_inf= models.IntegerField()
-    montant_depense_sup= models.IntegerField()
-    ca_inf= models.IntegerField()
-    ca_sup=models.FloatField()
+    date_debut = models.DateTimeField(null=True,blank=True) # A DATER
+    date_fin = models.DateTimeField(null=True,blank=True) # A DATER
+    exercice_sans_activite=models.BooleanField(null=True,blank=True)
+    nombre_activite = models.IntegerField(null=True,blank=True)
+    declaration_incomplete=models.BooleanField(null=True,blank=True)
+    date_publication = models.DateTimeField(null=True,blank=True) #A CHANGER EN DATE
+    exercice_sans_CA=models.BooleanField(null=True,blank=True)
+    montant_depense=models.CharField(null=True,blank=True,max_length=200)
+    nombre_salaries=models.CharField(null=True,blank=True,max_length=200)
+    chiffre_affaires = models.CharField(null=True,blank=True,max_length=200)
+    annee_debut= models.IntegerField(null=True,blank=True)
+    annee_fin= models.IntegerField(null=True,blank=True)
+    montant_depense_inf= models.IntegerField(null=True,blank=True)
+    montant_depense_sup= models.IntegerField(null=True,blank=True)
+    ca_inf= models.IntegerField(null=True,blank=True)
+    ca_sup=models.FloatField(null=True,blank=True)
 
 class Objets_activites(models.Model):
     activite_id = models.IntegerField(primary_key=True)
     exercices_id = models.ForeignKey(Exercices,on_delete=models.CASCADE)
-    date_publication_activite = models.CharField(max_length=200) #A CHANGER EN DATE
-    identifiant_fiche = models.CharField(max_length=200)
-    objet_activite= models.CharField(max_length=200)
+    date_publication_activite =models.DateTimeField(null=True,blank=True) #A CHANGER EN DATE
+    identifiant_fiche = models.CharField(null=True,blank=True,max_length=200)
+    objet_activite= models.CharField(null=True,blank=True,max_length=200)
 
 class Domaines_intervention(models.Model):
-    domaines_intervention_actions_menees=models.CharField(max_length=200)
+    domaines_intervention_actions_menees=models.CharField(null=True,blank=True,max_length=200)
     activite_id = models.ForeignKey(Objets_activites,on_delete=models.CASCADE)
 
 class Observations(models.Model):
     action_representation_interet_id = models.IntegerField(primary_key=True)
     activite_id = models.ForeignKey(Objets_activites,on_delete=models.CASCADE)
-    observation = models.CharField(max_length=200)
+    observation = models.CharField(null=True,blank=True,max_length=200)
 
 class Decisions_concernees(models.Model):
-    decision_concernee = models.CharField(max_length=200)
+    decision_concernee = models.CharField(null=True,blank=True,max_length=200)
     action_representation_interet_id = models.ForeignKey(Observations,on_delete=models.CASCADE)
 
 class Beneficiaires(models.Model):
-    beneficiaire_action_menee = models.CharField(max_length=200)
+    beneficiaire_action_menee = models.CharField(null=True,blank=True,max_length=200)
     action_representation_interet_id = models.ForeignKey(Observations,on_delete=models.CASCADE)
-    action_menee_en_propre = models.IntegerField()
+    action_menee_en_propre = models.IntegerField(null=True,blank=True,)
 
 class Actions_menees(models.Model):
-    action_menee= models.CharField(max_length=200)
+    action_menee= models.CharField(null=True,blank=True,max_length=200)
     action_representation_interet_id = models.ForeignKey(Observations,on_delete=models.CASCADE)
-    action_menee_autre= models.CharField(max_length=200)
-    other1= models.CharField(max_length=200)
-    other2= models.CharField(max_length=200)
-    other3= models.CharField(max_length=200)
-    other4= models.CharField(max_length=200)
+    action_menee_autre= models.CharField(null=True,blank=True,max_length=200)
+    other1= models.CharField(null=True,blank=True,max_length=200)
+    other2= models.CharField(null=True,blank=True,max_length=200)
+    other3= models.CharField(null=True,blank=True,max_length=200)
+    other4= models.CharField(null=True,blank=True,max_length=200)
+
+class Secteur_activites(models.Model):
+    secteur_activite = models.CharField(null=True,blank=True,max_length=200)
+    representants_id = models.ForeignKey(Informations_generales,on_delete=models.CASCADE)
