1530d580084885aeed2e0917fad38877f6114c48
Sébastie Cosneau <sebastien.cosneau@outlook.com>
2020-01-30 17:04:58 +0100

    Rename columns first table

--- hatvp/dash_apps/simpleexample.py
+++ hatvp/dash_apps/simpleexample.py
@@ -35,13 +35,11 @@ engine_string = "postgresql+psycopg2://{user}:{password}@{host}/{database}".form
 engine = create_engine(engine_string)
 
 # read a table from database into pandas dataframe
-df_col_names = ['ID Représentants', 'Adresse', 'Code Postal', 'Dernière publication active', 'Date première publication', 'Déclaration d\'appartenance à une organisation', 'Déclarations tiers', 'Dénomination', 'Identifiant National', 'Activités publiées', 'Page Facebook', 'Page Linkedin', 'Page Twitter', 'Site Web', 'Nom usage HATVP', 'Pays', 'Sigle HATVP', 'Type d\'identifiant national', 'Ville', 'Label catégorie organisation']
-df = pd.read_sql_table('hatvp_informations_generales', engine)
+df1 = pd.read_sql_table('hatvp_informations_generales', engine)
+df_1 = df1.rename(columns={'representants_id': 'ID Représentants', 'adresse': 'Adresse', 'code_postal': 'Code Postal', 'derniere_publication_activite': 'Dernière publication active', 'date_premiere_publication': 'Date première publication', 'declaration_organisation_appartenance': 'Déclaration d\'appartenance à une organisation', 'declaration_tiers': 'Déclarations tiers', 'denomination': 'Dénomination', 'identifiant_national': 'Identifiant National', 'activites_publiees': 'Activités publiées', 'page_facebook': 'Page Facebook', 'page_linkedin': 'Page Linkedin', 'page_twitter': 'Page Twitter', 'site_web': 'Site Web', 'nom_usage_HATVP': 'Nom usage HATVP', 'pays': 'Pays', 'sigle_HATVP': 'Sigle HATVP', 'type_identifiant_national': 'Type d\'identifiant national', 'ville': 'Ville', 'label_categorie_organisation': 'Label catégorie organisation'})
 
+# Page size for the dash table
 PAGE_SIZE = 10
-
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = DjangoDash('SimpleExample')
 
@@ -66,7 +64,7 @@ app.layout = html.Div([
     html.Div([
             dcc.Dropdown(
                 id='cp',
-                options=[{'label': i, 'value': i} for i in df],
+                options=[{'label': i, 'value': i} for i in df_1],
                 placeholder='Sélectionner un champ',
                 value='Code postal'
             )
@@ -75,9 +73,9 @@ app.layout = html.Div([
     html.Div([
         dcc.Dropdown(
             id='id_national',
-            options=[{'label': j, 'value': j} for j in df['identifiant_national']],
-            value=df['identifiant_national'][0],
-            placeholder='identifiant_national',
+            options=[{'label': j, 'value': j} for j in df_1['Identifiant National']],
+            value=df_1['Identifiant National'][0],
+            placeholder='Identifiant National',
             clearable=False,
         ),
         html.Div(id='output_container')
@@ -87,12 +85,12 @@ app.layout = html.Div([
         dash_table.DataTable(
             id='table-for-informations',
             columns=[
-                {'name': 'denomination', 'id': 'denomination', 'deletable': True},
-                {'name': 'label_categorie_organisation', 'id': 'label_categorie_organisation', 'deletable': True},
-                {'name': 'adresse', 'id': 'adresse', 'deletable': True},
-                {'name': 'code_postal', 'id': 'code_postal', 'deletable': True},
-                {'name': 'ville', 'id': 'ville', 'deletable': True},
-                {'name': 'pays', 'id': 'pays', 'deletable': True},
+                {'name': 'Dénomination', 'id': 'Dénomination', 'deletable': True},
+                {'name': 'Label catégorie organisation', 'id': 'Label catégorie organisation', 'deletable': True},
+                {'name': 'Adresse', 'id': 'Adresse', 'deletable': True},
+                {'name': 'Code Postal', 'id': 'Code Postal', 'deletable': True},
+                {'name': 'Ville', 'id': 'Ville', 'deletable': True},
+                {'name': 'Pays', 'id': 'Pays', 'deletable': True},
             ],
             page_current=0,
             page_size=PAGE_SIZE,
@@ -102,7 +100,24 @@ app.layout = html.Div([
             sort_mode='single',
             sort_by=[]
         )
-    ])
+    ]),
+
+#    dcc.Graph(
+#        figure={
+#            'data': [
+#                go.Bar(
+#                    x=df['nom'],
+#                    y=df['nombres']
+#                ),
+#                go.Bar(
+#                    x=df['nom'],
+#                    y=df['argent']
+#                )
+#            ]
+
+#        }
+
+#    )
 ])
 
 # Code pour le premier dropdown
@@ -113,16 +128,16 @@ app.layout = html.Div([
     dash.dependencies.Output('output_container', 'children'),
     [dash.dependencies.Input('id_national', 'value')])
 def update_output(value):
-    index = find_index(df, value)
-    adresse_organisme = df['adresse'][index]
-    cp_organisme = df['code_postal'][index]
-    ville_organisme = df['ville'][index]
+    index = find_index(df_1, value)
+    adresse_organisme = df_1['Adresse'][index]
+    cp_organisme = df_1['Code Postal'][index]
+    ville_organisme = df_1['Ville'][index]
     return 'The address of the selected national id is "{}"'.format(adresse_organisme) + ', the postal code is "{}"'.format(cp_organisme) + ' and the city is "{}"'.format(ville_organisme)
 
 
 def find_index(dfobject, value):
     value_index = 0
-    for row in dfobject['identifiant_national']:
+    for row in dfobject['Identifiant National']:
         if row == value:
             return value_index
         value_index += 1
@@ -138,14 +153,14 @@ def find_index(dfobject, value):
     ])
 def update_table(page_current, page_size, sort_by):
     if len(sort_by):
-        dff_1 = df.sort_values(
+        dff_1 = df_1.sort_values(
             sort_by[0]['column_id'],
             ascending=sort_by[0]['direction'] == 'asc',
             inplace=False
         )
     else:
         # No sort is applied
-        dff_1 = df
+        dff_1 = df_1
     return dff_1.iloc[
         page_current*page_size:(page_current+ 1)*page_size
     ].to_dict('records')
