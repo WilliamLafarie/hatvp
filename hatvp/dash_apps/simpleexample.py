775cd33246a2ce08e04b0f42ed092786637e08fd
Willou <lafarie.william@gmail.com>
2020-01-31 09:46:54 +0100

    dashboard template modification

--- hatvp/dash_apps/simpleexample.py
+++ hatvp/dash_apps/simpleexample.py
@@ -84,24 +84,7 @@ PAGE_SIZE = 10
 app = DjangoDash('SimpleExample')
 
 app.layout = html.Div([
-    dcc.Markdown('''
-    #### HATVP Project
-
-    Travail réalisé par **[William Lafarie](https://www.linkedin.com/in/william-lafarie/?originalSubdomain=fr)**, **[Odom Ear](https://www.linkedin.com/in/odom-ear/)**, **[Sébastien Cosneau](https://www.linkedin.com/in/sébastien-cosneau-56b72a104/)** et **[Gauthier Magne](https://www.linkedin.com/in/gauthier-magne-7a0805127/)**
-
-    Ce projet a été réalisé dans le cadre du cours de Python dispensé par **[Anthony Baillard](https://www.linkedin.com/in/anthonybaillard/?originalSubdomain=fr)** à **[Hétic](https://www.hetic.net)**
-    ''',
-                 style={
-                     'textAlign': 'center',
-                     'color': 'red',
-                     'fontSize': 24,
-                     'fontFamily': 'open-sans',
-                     'fontWeight': 'normal',
-                     'fontVariant': 'small-caps',
-                     'textDecoration': 'underline'
-                 }),
-
-    html.Div([
+   html.Div([
         dcc.Dropdown(
             id='dp-df1',
             options=[{'label': i, 'value': i} for i in df_1],
